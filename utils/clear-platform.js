2b3face07dcbd8e2e2beed1b4f5e1391f3190dc8
mmyduckx <308303735@qq.com>
2021-07-12 17:50:17 +0800

    update clear-platform command for win64

--- utils/clear-platform.js
+++ utils/clear-platform.js
@@ -24,6 +24,9 @@ function clearUselessPlatform() {
         let clearDirectory = path.join(externalDir, "./win32");
         console.log(`  ${chalk.green('Remove directory: ')} ${clearDirectory}`);
         ensureRemove(clearDirectory);
+        clearDirectory = path.join(externalDir, "./win64");
+        console.log(`  ${chalk.green('Remove directory: ')} ${clearDirectory}`);
+        ensureRemove(clearDirectory);
     } else {
         // remove apple related 3rd-libs
         console.log(chalk.magenta(`==== Remove apple related 3rd-libs ====`));
